997c4f4c4335285cf39aa945ced2bf495b3ad575
Sanjay Gawai <sanjay.gawai@rescueclick.com>
2025-10-16 23:23:36 +0530

    fix smtp

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,57 @@
+// // server.js
 // import express from "express";
+// import nodemailer from "nodemailer";
+// import cors from "cors";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use(cors({ origin: "*" })); 
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // Contact API
+// app.post("/api/contact", async (req, res) => {
+//   try {
+//     const { name, email, phone, message } = req.body;
+
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER, // your Gmail
+//         pass: process.env.EMAIL_PASS, // app password
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: process.env.EMAIL_USER,
+//       to: "sammeddevelopers@gmail.com", // your company email
+//       subject: `New Inquiry from ${name}`,
+//       text: `
+//         Name: ${name}
+//         Email: ${email}
+//         Phone: ${phone}
+//         Message: ${message}
+//       `,
+//     };
+
+//     await transporter.sendMail(mailOptions);
+
+//     res.json({ success: true, message: "Enquiry sent successfully!" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ success: false, message: "Failed to send enquiry." });
+//   }
+// });
+
+// app.listen(PORT, () =>
+//   console.log(`🚀 Server running at http://localhost:${PORT}`)
+// );
+
+
+import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
 import dotenv from "dotenv";
