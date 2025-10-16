5a69f0ff086d39caf6f6379a3fd0bd2bbe22517e
Sanjay Gawai <sanjay.gawai@rescueclick.com>
2025-10-16 23:16:53 +0530

    added cors

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -45,7 +45,7 @@ const Contact = () => {
   
     // 2️⃣ Call API (does not block download)
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://sernity.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
